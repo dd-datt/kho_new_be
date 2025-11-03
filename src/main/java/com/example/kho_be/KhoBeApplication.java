db12972a99272713a8f3379a3e93a5b18601b1b5
Nguyen Van Dat <vdat319@gmail.com>
2025-11-03 08:11:31 +0700

    khoi tao BE

---
+++ src/main/java/com/example/kho_be/KhoBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kho_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KhoBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kho_be/KhoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kho_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
